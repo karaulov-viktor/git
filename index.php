f45cfc4058445cc7d7f0f41527d3c42be558a2ac
Karaulov Viktor <karaylov-viktor@mail.ru>
2020-09-14 19:22:05 +0300

    + php, $title= 'My title'

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
+<?php
+
+$title = 'new page';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Документ</title>
+    <title><?=$title;?></title>
 </head>
 <body>
-    <h1>Заголовок</h1>>
+    <h1>Заголовок</h1>
 </body>
 </html>
